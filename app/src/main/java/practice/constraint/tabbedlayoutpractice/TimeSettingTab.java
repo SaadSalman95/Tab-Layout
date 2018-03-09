b27f00f69a3ae03b7162738770dbc00c2c176ce7
saad_ <saad.salman958@gmail.com>
2018-03-09 09:08:12 +0500

    Tab Layout

---
+++ app/src/main/java/practice/constraint/tabbedlayoutpractice/TimeSettingTab.java
@@ -0,0 +1,21 @@
+package practice.constraint.tabbedlayoutpractice;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by saad_ on 3/7/2018.
+ */
+
+public class TimeSettingTab extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.time_setting_fragment,container,false);
+    }
+}
+
